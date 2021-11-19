8c66d63429c4fcef7ec391faf51293d695cfa0d4
yafet segid <yafetsegid@yafets-MBP.attlocal.net>
2021-11-18 20:21:15 -0600

    creating portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFun() {
+  let darkMode = document.body;
+  darkMode.classList.toggle("dark_mode");
+}
+
+// function myFun() {
+//   let image = document.img;
+//   image.classList.toggle("imgToggle");
+// }
