0e151526ec217998125f3f3acd45b1a43bcef6c9
yafet segid <yafetsegid@yafets-MacBook-Pro.local>
2021-11-20 17:05:33 -0600

    adding contact page

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// let imgToggle = document.querySelector(".btn");
+
 function myFun() {
   let darkMode = document.body;
   darkMode.classList.toggle("dark_mode");
 }
 
-// function myFun() {
-//   let image = document.img;
-//   image.classList.toggle("imgToggle");
+// function imgToggle() {
+//   let imageDarkMode = document.imgToggle;
+//   imageDarkMode.classList.toggle("imgToggle");
 // }
+
+function phoneNumber() {
+  let result = confirm("Call : 713-922-8269");
+  if (result == true) {
+    window.location = "tel:713-922-8236";
+  }
+}
