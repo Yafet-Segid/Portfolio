b1cf3aad686f37c90bad85301f37de2efa089ead
yafet segid <yafetsegid@gmail.com>
2021-12-14 19:55:28 -0600

    clear code

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ function myFun() {
   darkMode.classList.toggle("dark_mode");
 }
 
-function resumePopUp() {
-  document.getElementById("popResume").classList.toggle("active");
-}
-
 // function myFun() {
 //   let image = document.img;
 //   image.classList.toggle("imgToggle");
